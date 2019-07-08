c54abbe1ab3a3a637155888b63a589a3f99777d1
Aden Cross <adenrudy23@gmail.com>
2019-07-07 17:00:33 -0700

    Unbound timed drive

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -26,9 +26,7 @@ public class OI {
     return (stick);
   }
 
-  public OI(){
-    trigger.whenPressed(new TimedDrive(2));
-  }
+  
 
   //// CREATING BUTTONS
   // One type of button is a joystick button which is any button on a
